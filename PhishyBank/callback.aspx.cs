a0c6a18b42207ac98d7779254811356e575c73e3
Soh Thiam Hing <daffodilistic@gmail.com>
2020-06-04 16:28:20 +0800

    Use 2 different apps (one for debugging and the other for production), as Fidor only supports one app site

--- PhishyBank/callback.aspx.cs
+++ PhishyBank/callback.aspx.cs
@@ -10,12 +10,28 @@ namespace PhishyBank
 {
     public partial class callback : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        OAuth2 getFidorOauth2Container()
         {
             OAuth2 oauthContainer = new OAuth2();
-            oauthContainer.client_id = "911de6ba62ca08d8";
-            oauthContainer.client_secret = "bfc36cdf50727803bf8bf8677fd0d1ac";
-            oauthContainer.redirect_uri = new System.Uri(Page.Request.Url, "/callback.aspx").AbsoluteUri;
+            if (this.Context.IsDebuggingEnabled)
+            {
+                oauthContainer.client_id = "062e071527f30139";
+                oauthContainer.client_secret = "aeae6e5fc945e322d821391418dac8f7";
+                oauthContainer.redirect_uri = "http://localhost:3000/callback.aspx";
+            }
+            else
+            {
+                oauthContainer.client_id = "911de6ba62ca08d8";
+                oauthContainer.client_secret = "bfc36cdf50727803bf8bf8677fd0d1ac";
+                oauthContainer.redirect_uri = "https://phishybank.site/callback.aspx";
+            }
+
+            return oauthContainer;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            OAuth2 oauthContainer = getFidorOauth2Container();
 
             string responseCode = Request.QueryString["code"];
             APIAccess fidorApi = new APIAccess();
