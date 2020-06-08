2129d63c60225a3d9c640030c5a8e9bde6e06413
Soh Thiam Hing <daffodilistic@gmail.com>
2020-06-08 21:53:03 +0800

    Refactor email sending code into PhishyAPI.

--- Phishy/classes/PhishyAPI.cs
+++ Phishy/classes/PhishyAPI.cs
@@ -1,10 +1,15 @@
-﻿using FintechAPI;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
+
+using FintechAPI;
+using PostmarkDotNet;
+using PostmarkDotNet.Model;
+
 
 namespace Phishy
 {
@@ -44,6 +49,54 @@ namespace Phishy
 
             return authResult;
         }
+
+        internal async void SendNotificationEmail(
+            String recipient="nemo@phishybank.site",
+            String senderName="Phishy Bank",
+            Double amount=1.88)
+        {
+            String formattedAmount = amount.ToString("C2", CultureInfo.CreateSpecificCulture("en-SG"));
+            String formattedTime = DateTime.Now.ToString(new CultureInfo("en-SG"));
+
+            Dictionary<string, string> templateModelData = new Dictionary<string, string>() {
+                { "product_url", "https://www.phishybank.site" },
+                { "product_name", "Phishy Bank" },
+                { "sender_name", senderName },
+                { "amount", formattedAmount },
+                { "time", formattedTime },
+                { "action_url", "https://www.phishybank.site" },
+                { "company_name", "Phishy Bank" },
+                { "company_address", "123 Sesame Street" }
+            };
+
+            // Send an email asynchronously:
+            var message = new TemplatedPostmarkMessage()
+            {
+                TemplateId = 18352791,
+                TemplateModel = templateModelData,
+                InlineCss = true,
+                From = "noreply@phishybank.site",
+                To = recipient,
+                Bcc = "nemo@phishybank.site",
+                TrackOpens = true
+            };
+
+            //var imageContent = File.ReadAllBytes("test.jpg");
+            //message.AddAttachment(imageContent, "test.jpg", "image/jpg", "cid:embed_name.jpg");
+
+            var client = new PostmarkClient("e8603c27-0841-47fe-8cc6-daf4163de446");
+            var sendResult = await client.SendEmailWithTemplateAsync(message);
+
+            if (sendResult.Status == PostmarkStatus.Success)
+            {
+                // Do something
+            }
+            else
+            {
+                // Do something
+            }
+        }
+
         internal Task<Customers> GetTransferHistory()
         {
             return base.GetCustomers(PhishyAPI.accessToken);
