d7435d1e9989a1cf37709f6713ba6e6b6e1481db
Soh Thiam Hing <daffodilistic@gmail.com>
2020-06-10 00:10:51 +0800

    Use requested URL to pass to Fidor, instead of hardcoding

--- Phishy/classes/PhishyHelper.cs
+++ Phishy/classes/PhishyHelper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 
 namespace Phishy
@@ -16,16 +17,20 @@ namespace Phishy
             OAuth2 oauthContainer = new OAuth2();
             if (isDebug)
             {
+                string redirectUri = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/callback.aspx";
+
                 oauthContainer.client_id = "062e071527f30139";
                 oauthContainer.client_secret = "aeae6e5fc945e322d821391418dac8f7";
                 // NOTE: Must match!
-                oauthContainer.redirect_uri = "http://localhost:3000/callback.aspx";
+                oauthContainer.redirect_uri = redirectUri;
             }
             else
             {
+                string redirectUri = HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority) + "/callback.aspx";
+
                 oauthContainer.client_id = "911de6ba62ca08d8";
                 oauthContainer.client_secret = "bfc36cdf50727803bf8bf8677fd0d1ac";
-                oauthContainer.redirect_uri = "https://phishybank.site/callback.aspx";
+                oauthContainer.redirect_uri = redirectUri;
             }
 
             return oauthContainer;
