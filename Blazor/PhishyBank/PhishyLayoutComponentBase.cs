79a33e3e1899fb90c26b4a26ba830f864940afcf
Soh Thiam Hing <daffodilistic@gmail.com>
2022-07-26 21:55:23 +0800

    [refactor] Inherit from PhishyLayoutComponentBase, and moved index page HTML markup into Index.razor

---
+++ Blazor/PhishyBank/PhishyLayoutComponentBase.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components;
+
+namespace PhishyBank
+{
+    public class PhishyLayoutComponentBase : LayoutComponentBase
+    {
+        public static string getPageTitle(string title = null)
+        {
+            if (title == null)
+            {
+                return "PhishyBank";
+            }
+            else
+            {
+                return "PhishyBank - " + title;
+            }
+        }
+    }
+}
